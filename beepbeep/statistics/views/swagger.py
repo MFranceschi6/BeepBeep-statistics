805cc5a866a8fdb1e89cc083e02ec094c4b9e9ed
DanyEle <daniele.gadler@yahoo.it>
2018-11-29 15:12:20 +0100

    Fixed / and missing values for attributes in runs

--- beepbeep/statistics/views/swagger.py
+++ beepbeep/statistics/views/swagger.py
@@ -49,15 +49,22 @@ def get_all_statistics_user_id(user_id):
         for attr in run_response:
             attr_value = run_response[attr]
 
-            if(attr == "distance"):
+            #if an attribute is found to have an invalid value, it is set to 0 to avoid wrong displays in the webservice JS
+            if(attr_value == None or attr_value == "" or attr_value == "Null"):
+                attr_value = 0
+
+            if(attr == "distance" ):
                 distance_array.append(attr_value)
-            elif(attr == "average_speed"):
+            #if invalid distance:
+            elif(attr=="distance" and (attr_value == "0" or attr_value == None)):
+                distance_array.append(0)
+            elif(attr == "average_speed" ):
                 average_speed_array.append(attr_value)
-            elif(attr == "average_heartrate"):
+            elif(attr == "average_heartrate"  and attr_value > 0):
                 average_heart_rate_array.append(attr_value)
-            elif(attr == "total_elevation_gain"):
+            elif(attr == "total_elevation_gain" ):
                 elevation_gain_array.append(attr_value)
-            elif(attr == "elapsed_time"):
+            elif(attr == "elapsed_time" ):
                 elapsed_time_array.append(attr_value)
             elif(attr == "title"):
                 run_names_array.append(attr_value)
@@ -121,13 +128,17 @@ def get_single_statistics_user_id(user_id, statistics_name):
     for run_response in runs_response:
         for attr in run_response:
             attr_value = run_response[attr]
-            if(attr == "distance" and statistics_name == "distances"):
+
+            if (attr_value == None or attr_value == "" or attr_value == "Null"):
+                attr_value = 0
+
+            if(attr == "distance" and statistics_name == "distances" ):
                 distance_array.append(attr_value)
-            elif(attr == "average_speed" and statistics_name == "average_speeds"):
+            elif(attr == "average_speed" and statistics_name == "average_speeds" ):
                 average_speed_array.append(attr_value)
             elif(attr == "average_heartrate" and statistics_name == "average_heart_rates"):
                 average_heart_rate_array.append(attr_value)
-            elif(attr == "total_elevation_gain" and statistics_name == "elevation_gains"):
+            elif(attr == "total_elevation_gain" and statistics_name == "elevation_gains" ):
                 elevation_gain_array.append(attr_value)
             elif(attr == "elapsed_time" and statistics_name == "elapsed_times"):
                 elapsed_time_array.append(attr_value)
